2189c202cbc8b3a0e5a8b934dc471ad2708e26b7
Maksym Chesherov <chesherow22d@gmail.com>
2025-06-10 17:22:47 +0300

    changed to async approach

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,37 +1,25 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+import asyncio
+from asyncio import Future
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from app.scraper import scrape
 from app.config import SCRAPING_TIME, DUMP_TIME
-import subprocess
-import os
-import datetime
-
-def dump_db():
-    print("[DUMPER] Dump started...")
-
-    now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    filename = f"dumps/dump_{now}.sql"
-    os.makedirs("dumps", exist_ok=True)
-    subprocess.call([
-        "pg_dump",
-        "-U", "postgres",
-        "-h", "db",
-        "-f", filename,
-        "autoria"
-    ])
-    print(f"Database dumped to {filename}")
+from app.dump import dump_db
 
 
-def start():
-    scheduler = BlockingScheduler()
-    hour, minute = map(int, SCRAPING_TIME.split(":"))
-    scheduler.add_job(scrape, 'cron', hour=hour, minute=minute)
+async def start():
+    scheduler = AsyncIOScheduler()
+    h, m = map(int, SCRAPING_TIME.split(":"))
+    scheduler.add_job(scrape, "cron", hour=h, minute=m)
 
-    dhour, dminute = map(int, DUMP_TIME.split(":"))
-    scheduler.add_job(dump_db, 'cron', hour=dhour, minute=dminute)
+    dh, dm = map(int, DUMP_TIME.split(":"))
+    scheduler.add_job(dump_db, "cron", hour=dh, minute=dm)
 
-    print("Scheduler started")
     scheduler.start()
+    print("Scheduler started")
+
+    await Future()
 
 
 if __name__ == "__main__":
-    start()
+    asyncio.run(start())
