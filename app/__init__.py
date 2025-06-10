8f8fa0bbd709580718d0b3704dd2fe4e90d78ab7
Maksym Chesherov <chesherow22d@gmail.com>
2025-06-10 17:20:24 +0300

    small fixes

---
+++ app/__init__.py
@@ -0,0 +1,3 @@
+from .db import get_session
+from .scraper import scrape
+from .dump import dump_db
--- app/config.py
+++ app/config.py
@@ -1,9 +1,7 @@
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SCRAPING_TIME = os.getenv("SCRAPING_TIME")
 DUMP_TIME = os.getenv("DUMP_TIME")
 START_URL = os.getenv("START_URL")
+MAX_PAGES = int(os.getenv("MAX_PAGES"))
