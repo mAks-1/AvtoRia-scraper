64e5bff1d53fe07768353765786cd76045bd2a30
Maksym Chesherov <chesherow22d@gmail.com>
2025-06-10 17:20:02 +0300

    changed to async session

--- app/db.py
+++ app/db.py
@@ -1,6 +1,10 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(DATABASE_URL, echo=False)
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
+
+async def get_session():
+    return AsyncSessionLocal()
